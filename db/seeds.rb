743f07506b400e31bd1767d3b8cb5e7dd6eb0a7f
Ed Mckain <mckain.default@gmail.com>
2016-08-22 20:39:49 -0600

    creates default client from Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Labadee Seeds
 
 # Creates default admin user.
-admin_user = User.create(email: 'admin@admin.com', password: 'secretpassword')
+User.create(email: 'admin@admin.com', password: 'secretpassword')
+
+# Creates default client.
+Client.create(name: 'mr-roboto', description: 'Default test client')
